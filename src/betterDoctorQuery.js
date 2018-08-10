bb6a716716ce2cb3169a0304c3b795791bfbebcb
Renee Sarley <reneesarley@gmail.com>
2018-08-10 13:23:05 -0700

    add getKnownConditions method

---
+++ src/betterDoctorQuery.js
@@ -0,0 +1,36 @@
+let Promise = require('es6-promise').Promise;
+
+export class BetterDoctorQuery {
+
+  getDoctorsByMedicalIssue() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+  getKnownConditions() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/conditions?limit=100&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+}
--- src/main.js
+++ src/main.js
@@ -2,20 +2,28 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { DoctorLookup } from './DoctorLookup.js'
+import { BetterDoctorQuery } from './betterDoctorQuery.js'
 // let Promise = require('es6-promise').Promise;
 
 $(document).ready(function(){
   console.log("this is working");
 
-  let doctorLookup = new DoctorLookup();
-  let promise = doctorLookup.getDoctorsByMedicalIssue();
-  promise.then(function(response) {
+  let betterDoctorQuery = new BetterDoctorQuery();
+  let promiseGetDoctors = betterDoctorQuery.getDoctorsByMedicalIssue();
+  let promiseGetConditions = betterDoctorQuery.getKnownConditions();
+  promiseGetDoctors.then(function(response) {
     let body = JSON.parse(response);
-    console.log("the promise is running");
+    console.log("the get doctor promise is running");
     console.log(body.data[1]);
     // $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
     // $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+  }, function(error) {
+    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+  });
+  promiseGetConditions.then(function(response) {
+    let body = JSON.parse(response);
+    console.log("the get conditions promise is running");
+    console.log(body.data[0].name);
   }, function(error) {
     $('.showErrors').text(`There was an error processing your request: ${error.message}`);
   });
